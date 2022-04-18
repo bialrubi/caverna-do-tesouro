8164629bd322263254756f8d45e8451ebc7a61cc
bialrubi <93795480+bialrubi@users.noreply.github.com>
2022-04-18 17:52:37 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Executa uma tarefa em particular", 700, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Armazena todas as informações", 100, 270);
+  
+}
